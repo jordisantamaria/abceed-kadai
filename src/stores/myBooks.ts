db4e49cc1565e5a1cbf3191e736c213fcfcd8e53
jordisantamaria <jordisantamaria1a@gmail.com>
2025-01-01 15:01:22 +0900

    refactor

--- src/stores/myBooks.ts
+++ src/stores/myBooks.ts
@@ -1,20 +1,22 @@
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useMyBooksStore = defineStore('myBooks', {
-  state: () => ({
-    myBooks: [] as string[],
-  }),
-  actions: {
-    addToMyBooks(bookId: string) {
-      if (!this.myBooks.includes(bookId)) {
-        this.myBooks.push(bookId)
-      }
-    },
-    removeFromMyBooks(bookId: string) {
-      this.myBooks = this.myBooks.filter((id) => id !== bookId)
-    },
-    isInMyBooks(bookId: string) {
-      return this.myBooks.includes(bookId)
-    },
-  },
+export const useMyBooksStore = defineStore('myBooks', () => {
+  const myBooks = ref([] as string[])
+
+  const addToMyBooks = (bookId: string) => {
+    if (!myBooks.value.includes(bookId)) {
+      myBooks.value.push(bookId)
+    }
+  }
+
+  const removeFromMyBooks = (bookId: string) => {
+    myBooks.value = myBooks.value.filter((id) => id !== bookId)
+  }
+
+  const isInMyBooks = (bookId: string) => {
+    return myBooks.value.includes(bookId)
+  }
+
+  return { myBooks, addToMyBooks, removeFromMyBooks, isInMyBooks }
 })
